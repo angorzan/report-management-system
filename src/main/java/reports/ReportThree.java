56de4c86a0eb96638313c7fd35e7efa58bb4215b
Anna Gorzanowska <anna.gorzanowska@gmail.com>
2021-06-20 11:27:01 +0200

    move sample data to tests folder

---
+++ src/main/java/reports/ReportThree.java
@@ -0,0 +1,14 @@
+package reports;
+
+import reportManagement.Menu;
+import reportManagement.ProjectTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReportThree<list> {
+
+
+
+}
