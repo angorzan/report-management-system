5e0868454d9f65729103e0aba1fcefe9933465e3
Kasia13130 <kuckatarzyna399@gmail.com>
2021-06-20 11:37:54 +0200

    Print report table

--- src/main/java/reportManagement/App.java
+++ src/main/java/reportManagement/App.java
@@ -3,7 +3,7 @@ package reportManagement;
 public class App {
 	public static void main(String[] args) {
 		
-		Menu menu = new Menu("src/main/Resources/Kowalski_Jan.xls");
+		Menu menu = new Menu("src/main/java/Resources/Kowalski_Jan.xls");
 		menu.printMainMenu();
 
 	}
--- src/main/java/reportManagement/PrintTables.java
+++ src/main/java/reportManagement/PrintTables.java
@@ -13,32 +13,32 @@ public class PrintTables {
 		String name2 = "Kwiatkowska Monika";
 		String projectName = "Nazwa projektu";
 		int iloscGodzin = 30;
+		char star = '*';
 		
 		System.out.println("Wyświetlenie raportu godzin pracownikow w danym roku:");
-		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);		// wedlug tej lini wykonac tabele
-		System.out.printf("\nLp%21s %24s %15s",name2, projectName, iloscGodzin);
-		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);
-		System.out.printf("\nLp%15s %30s %15s\n",name, projectName, iloscGodzin);
+		System.out.println("Lp\t\tNazwisko i imię\t\t\th/rok");
+		System.out.printf("1%35s %30s %15s",name, projectName, iloscGodzin);		
+		System.out.println(star * 30);
 		
-		System.out.printf("\n\nWyswietlenie raportu godzin projektowych w danym roku:");
+		System.out.printf("\n\nWyświetlenie raportu godzin projektowych w danym roku:");
+		System.out.println("\nLp\t\t\tNazwa projektu\t\t\tilość godzin");
 		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);
-		System.out.printf("\nLp%21s %24s %15s",name2, projectName, iloscGodzin);
-		System.out.printf("\nLp%15s %30s %15s\n",name, projectName, iloscGodzin);
 		
-		System.out.printf("\n\nWy�wietlenie miesi�cznego raportu godzin przepracowanych przez jednego pracownika:");
-		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);
-		System.out.printf("\nLp%21s %24s %15s",name2, projectName, iloscGodzin);
-		System.out.printf("\nLp%15s %30s %15s\n",name, projectName, iloscGodzin);
 		
-		System.out.printf("\n\nWy�wietlenie raportu procentowego udzia�u godzinowego w projektach dla danego pracownika:");
+		System.out.printf("\n\nWyświetlenie miesięcznego raportu godzin przepracowanych przez jednego pracownika:");
+		System.out.println("\nLp\t\t\tMiesiąc\t\t\tProjekt\t\t\tlp. godzin");
 		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);
-		System.out.printf("\nLp%21s %24s %15s",name2, projectName, iloscGodzin);
-		System.out.printf("\nLp%15s %30s %15s\n",name, projectName, iloscGodzin);
 		
-		System.out.printf("\n\nWy�wietlenie raportu sumarycznego godzin projektowych:");
+		
+		System.out.printf("\n\nWyświetlenie raportu procentowego udziału godzinowego w projektach dla danego pracownika:");
+		System.out.println("\nLp\t\t\tNazwisko i imię\t\t\tProjekt 1\t\t\tUdział procentowy\t\t\tProjekt 2\t\t\tUdział procentowy");
 		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);
-		System.out.printf("\nLp%21s %24s %15s",name2, projectName, iloscGodzin);
-		System.out.printf("\nLp%15s %30s %15s\n",name, projectName, iloscGodzin);
+		
+		
+		System.out.printf("\n\nWyświetlenie raportu sumarycznego godzin projektowych:");
+		System.out.println("\nLp\t\t\tNazwisko i imię\t\t\tLączna suma godzin");
+		System.out.printf("\nLp%15s %30s %15s",name, projectName, iloscGodzin);
+		
 		
 	}
 	
