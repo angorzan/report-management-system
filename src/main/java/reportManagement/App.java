c09c1838afb101976c84d11073d468f24d6aa504
Maciek Blok <mblok@student.agh.edu.pl>
2021-06-20 15:45:00 +0200

    Menu integration in progress

--- src/main/java/reportManagement/App.java
+++ src/main/java/reportManagement/App.java
@@ -9,14 +9,14 @@ public class App {
 	public static void main(String[] args) {
 
 		Menu menu = new Menu("../report-management-system/src/test/resources");
-//		menu.printMainMenu();
+		menu.printMainMenu();
 		
-		EmployeeAlphabeticalReport.printReport(2012);	//raport 1
-
-		ProjectSummaryHoursReport.printReport(2012);	// raport2
-
-		EmployeeDetailedAnnualReport.printReport("Jan Kowalski", 2012);	// raport 3
-
-		ProjectEmployeeConsumptionReport.printReport("Projekt2");	// raport 5
+//		EmployeeAlphabeticalReport.printReport(2012);	//raport 1
+//
+//		ProjectSummaryHoursReport.printReport(2012);	// raport2
+//
+//		EmployeeDetailedAnnualReport.printReport("Jan Kowalski", 2012);	// raport 3
+//
+//		ProjectEmployeeConsumptionReport.printReport("Projekt2");	// raport 5
 	}
 }
