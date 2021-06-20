4605c099418e48c0c925dbe9868f3625f9ad71ca
Anna Gorzanowska <anna.gorzanowska@gmail.com>
2021-06-20 11:30:04 +0200

    move sample data to tests folder

--- src/main/java/reportManagement/App.java
+++ src/main/java/reportManagement/App.java
@@ -3,7 +3,7 @@ package reportManagement;
 public class App {
 	public static void main(String[] args) {
 		
-		Menu menu = new Menu("src/test/java/resources/reporter-dane");
+		Menu menu = new Menu("src/test/resources/reporter-dane");
 		menu.printMainMenu();
 
 	}
