d104797f177dbb2f1c86386239975a147dc3ce87
Maciek <maciekblok@gmail.com>
2021-06-22 03:58:48 +0200

    missing MonthsConversionTable class added

---
+++ src/main/java/utilities/MonthsConversionTable.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class MonthsConversionTable {
+
+	public static final String[] MONTHS_NAME = {"Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Padziernik", "Listopad", "Grudzień"};
+	
+}
